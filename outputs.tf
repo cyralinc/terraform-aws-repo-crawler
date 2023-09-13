1847af47abeb7c0afacab636ac930fdcfe4fcaf6
Miguel Bernadin <miguel.bernadin@gmail.com>
2023-09-13 09:22:07 -0700

    feat: add cyral output var repo_crawler_aws_security_group_id (#4)
    Adds the Repo Crawler security group ID as a Terraform output.

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "repo_crawler_lambda_function_arn" {
   value = aws_lambda_function.this.arn
   description = "The Amazon Resource Name (ARN) of the Repo Crawler Lambda function."
 }
+
+output "repo_crawler_aws_security_group_id" {
+  value = aws_security_group.this.id
+  description = "The Amazon Security Group ID of the Repo Crawler Lambda function."
+}
